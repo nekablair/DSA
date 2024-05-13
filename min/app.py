92ee8f29e32e75ff61e22bb7b2f769cc0423b728
Neka Blair <ourblairfamily@gmail.com>
2024-05-13 08:32:31 -0500

    update

---
+++ min/app.py
@@ -0,0 +1,28 @@
+# Given an sorted integer array, nums, that has been rotated at an unknown index, return the minimum value.
+# Note: The array only contains unique values.
+
+# Ex: Given the following array nums...
+
+# nums = [7, 9, 10, 2, 4, 6], return 2.
+# PREP
+# Parameter: None
+# Return: smallest value from list
+# Example: nums = [7, 9, 10, 2, 4, 6], return 2.
+# Pseudocode:
+# - iterate through list
+# - return smallest value from list
+# -
+
+
+nums = [7, 9, 10, 2, 4, 6]
+
+def find_min():
+    # print(nums)
+    return nums
+    # for num in nums:
+    #     print(num)
+
+
+
+
+print(find_min)
